4a4f97887aa7c90efcd6aba8e926fbc4f01f9d50
Taimur Shaikh <taimurshaikh2004@gmail.com>
2020-10-04 08:13:28 +0400

    Initial commit

---
+++ stack.py
@@ -0,0 +1,47 @@
+class Stack:
+  def __init__(self, size):
+    self._size = size
+    self._arr = []
+    self._head = -1
+
+  def isFull(self):
+    return len(self._arr) == self._size
+  def isEmpty(self):
+    return self._head == -1
+
+  def push(self, value):
+      if not self.isFull():
+          self._arr.append(value)
+          self._head += 1
+      else:
+          print("\nSTACK FULL\n")
+
+  def pop(self):
+      if not self.isEmpty():
+          self._arr = self._arr[:-1]
+          self._head -= 1
+      else:
+          print("\nSTACK EMPTY\n")
+
+  def peek(self):
+      return self._arr[-1] if not self.isEmpty() else "STACK EMPTY"
+
+  def printStack(self):
+      for val in self._arr:
+          print(val, end=' ')
+      print("\n")
+
+ask = int(input("Length of stack: "))
+testStack = Stack(ask)
+print(testStack.isEmpty())
+print(testStack.isFull())
+testStack.push(1)
+testStack.push(2)
+testStack.push(3)
+testStack.push(14)
+testStack.push(15)
+testStack.push(16)
+testStack.printStack()
+testStack.pop()
+testStack.printStack()
+print(testStack.peek())
